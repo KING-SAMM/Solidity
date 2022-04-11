2af037709e3941a005d3ff549e0cd713d2e7c532
KING-Samm <kcsamm11@gmail.com>
2022-04-11 13:01:44 +0100

    Implemented: 1. Conversion of sent USD to Wei, 2. Minimum threshold amount in ETH to send

--- fund_me/FundMe.sol
+++ fund_me/FundMe.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.0 <0.9.0;
+pragma solidity ^0.6.0;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
 /**
 * @title FundMe
 * @dev This contract is payable
 */
 contract FundNe {
+    using SafeMathChainlink for uint256;
+
     // Keep track of who sent us some funding
     mapping(address => uint256) public addressToAmountFunded;
 
@@ -17,12 +20,19 @@ contract FundNe {
     */
     function fund() public payable
     {
+        // Setting a minimum threshold of 50 USD in wei
+        uint256 minimumUSD = 50 * 10 ** 18;
+
+        // If amount sent is not up to minimum required 
+        // then revert with an error message
+        require(getConversionRate(msg.value) >= minimumUSD, "You need to send at least $50 in ETH");
+
         // Keep track of who sent us some funding
         addressToAmountFunded[msg.sender] += msg.value;
         
     }
 
-    /** 
+    /**
     * @dev Function to call .version()
     */
     function getVersion() public view returns(uint256)
@@ -31,7 +41,7 @@ contract FundNe {
         return pricefeed.version();
     }
 
-    /** 
+    /**
     * @dev Function to call .price()
     */
     function getPrice() public view returns (uint256)
@@ -39,6 +49,7 @@ contract FundNe {
         AggregatorV3Interface pricefeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         ( , int256 answer, , , ) = pricefeed.latestRoundData();
         return uint256( answer * 10000000000 );
+        // uint256: 327362859828
     }
 
     /**
